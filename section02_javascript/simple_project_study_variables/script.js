0d3592df4b455e8a943149462d28e1802f15b859
Bruno <brunocosta.1991@hotmail.com>
2024-11-14 21:53:00 -0300

    simple project to study variables into javascript

---
+++ section02_javascript/simple_project_study_variables/script.js
@@ -0,0 +1,5 @@
+var firstname = prompt("What's your name? ")
+var lastname  = prompt("What's your last name? ")
+
+document.write(`<h1>Welcome to the project</h1><p>Name: ${firstname}, </br>Lastname: ${lastname}</p>`)
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='image Steve Jobs' />")
