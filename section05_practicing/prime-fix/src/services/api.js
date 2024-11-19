4a5d6f7c37a92f01d74ca00d47d77e6f60db0571
Bruno <brunocosta.1991@hotmail.com>
2024-11-19 20:02:35 -0300

    fix apikey to env, and alter apikey into server

--- section05_practicing/prime-fix/src/services/api.js
+++ section05_practicing/prime-fix/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: "81ffb4c1be9af05572332d4e8b876556",
+    api_key: `${import.meta.env.VITE_REACT_APP_TMDB_API_KEY}`,
   }
 });
 
